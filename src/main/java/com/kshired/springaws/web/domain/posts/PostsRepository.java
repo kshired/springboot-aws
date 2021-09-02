d67a561d196a7b7e44eaeec79db36abbc953c7ad
kshired <shiroed1211@naver.com>
2021-09-03 05:47:04 +0900

    Chapter #4 finished

--- src/main/java/com/kshired/springaws/web/domain/posts/PostsRepository.java
+++ src/main/java/com/kshired/springaws/web/domain/posts/PostsRepository.java
@@ -2,5 +2,8 @@ package com.kshired.springaws.web.domain.posts;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface PostsRepository extends JpaRepository<Posts, Long> {
+    List<Posts> findAllByOrderByIdDesc();
 }
--- src/main/java/com/kshired/springaws/web/service/posts/PostsService.java
+++ src/main/java/com/kshired/springaws/web/service/posts/PostsService.java
@@ -2,13 +2,16 @@ package com.kshired.springaws.web.service.posts;
 
 import com.kshired.springaws.web.domain.posts.Posts;
 import com.kshired.springaws.web.domain.posts.PostsRepository;
+import com.kshired.springaws.web.dto.PostsListResponseDto;
 import com.kshired.springaws.web.dto.PostsResponseDto;
 import com.kshired.springaws.web.dto.PostsSaveRequestDto;
 import com.kshired.springaws.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
-import javax.transaction.Transactional;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Service
@@ -16,14 +19,14 @@ public class PostsService {
     private final PostsRepository postsRepository;
 
     @Transactional
-    public Long save(PostsSaveRequestDto requestDto){
+    public Long save(PostsSaveRequestDto requestDto) {
         return postsRepository.save(requestDto.toEntity()).getId();
     }
 
     @Transactional
     public Long update(Long id, PostsUpdateRequestDto requestDto) {
         Posts posts = postsRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id=" + id));
-        posts.update(requestDto.getTitle(),requestDto.getContent());
+        posts.update(requestDto.getTitle(), requestDto.getContent());
 
         return id;
     }
@@ -32,5 +35,18 @@ public class PostsService {
         Posts entity = postsRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id=" + id));
 
         return new PostsResponseDto(entity);
+    }
+
+    @Transactional(readOnly=true)
+    public List<PostsListResponseDto> findAllDesc() {
+        return postsRepository.findAllByOrderByIdDesc().stream()
+                .map(PostsListResponseDto::new)
+                .collect(Collectors.toList());
+    }
+
+    @Transactional
+    public void delete(Long id){
+        Posts posts = postsRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id=" + id));
+        postsRepository.delete(posts);
     }
 }
